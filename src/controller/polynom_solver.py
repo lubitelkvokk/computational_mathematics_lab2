13312daabc85fbdee3d54f139e4d36f066d15c09
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2024-03-04 23:41:19 +0300

    fixed simple iteration method

--- src/controller/polynom_solver.py
+++ src/controller/polynom_solver.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
 
-from src.service.polynom_solver_service import solve_with_chords, solve_with_newton
+from src.service.polynom_solver_service import solve_with_chords, solve_with_newton, solve_with_simple_iteration
 from src.util.plot_generator import generate_plot
 
 router = APIRouter()
@@ -25,9 +25,13 @@ async def plot_graph(request: Request, k_for_x_3: float = Form(...), k_for_x_2: 
     # result = solve_with_chords(left_board, right_board,
     #                            [k_for_constant, k_for_x_1, k_for_x_2, k_for_x_3],
     #                            accuracy)
-    result = solve_with_newton(left_board, right_board,
-                               [k_for_constant, k_for_x_1, k_for_x_2, k_for_x_3],
-                               accuracy)
+
+    try:
+        result = solve_with_simple_iteration(left_board, right_board,
+                                   [k_for_constant, k_for_x_1, k_for_x_2, k_for_x_3],
+                                   accuracy)
+    except Exception as e:
+        result = e
     print(result)
     return templates.TemplateResponse("polynom_solver.html",
                                       {"request": request, "plot_data": "static/images/image.png", "result": result})
--- src/service/polynom_solver_service.py
+++ src/service/polynom_solver_service.py
@@ -1,5 +1,6 @@
 from src.util.chord_method import find_solution as find_solution_with_chord_method
 from src.util.newton_method import find_solution as find_solution_with_newton_method
+from src.util.simple_iteration_method import find_solution as find_solution_with_simple_iteration_method
 from src.util.isolation_intervals import get_isolation_intervals
 
 
@@ -42,5 +43,23 @@ def solve_with_newton(a: float, b: float, koeffs, accuracy: float):
         for i in intervals:
             root_number += 1
             x_i, iteration_count = find_solution_with_newton_method(i[0], i[1], koeffs, accuracy)
+            result += f"{root_number} root: f({x_i}) = {func(x_i)}, iteration count: {iteration_count}\n"
+        return result
+
+
+def solve_with_simple_iteration(a: float, b: float, koeffs, accuracy: float):
+    func = lambda x: sum(koeff * x ** i for i, koeff in enumerate(koeffs))
+    intervals = get_roots_intervals(a, b, 0.1, func)
+    result = ""
+
+    if len(intervals) == 0:
+        result += "There are no roots on this interval"
+        return result
+    else:
+        root_number = 0
+        result += f"Found roots:\n"
+        for i in intervals:
+            root_number += 1
+            x_i, iteration_count = find_solution_with_simple_iteration_method(i[0], i[1], koeffs, accuracy)
             result += f"{root_number} root: f({x_i}) = {func(x_i)}, iteration count: {iteration_count}\n"
         return result
